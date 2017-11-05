0b46944c9fda178309eb97df2b10d52648ac44be
raquelitarosaguilar <raquelita15@ru.is>
2017-11-05 23:08:01 +0000

    The whole project -initial commit

---
+++ cmake-build-debug/CMakeFiles/pa3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa3.dir/src/http_get.c.o"
+  "CMakeFiles/pa3.dir/src/http_head.c.o"
+  "CMakeFiles/pa3.dir/src/http_post.c.o"
+  "CMakeFiles/pa3.dir/src/http_server.c.o"
+  "CMakeFiles/pa3.dir/src/main.c.o"
+  "pa3.pdb"
+  "pa3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pa3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
